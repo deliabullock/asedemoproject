1c2b4e0146fa584c2dc7f0923f36e892eb29b4ea
Zion Lin <zion.e.lin@gmail.com>
2016-12-01 22:03:04 -0500

    Create Image.java

---
+++ initial/src/main/java/Image.java
@@ -0,0 +1,32 @@
+package hello;
+
+import org.springframework.data.annotation.Id;
+
+public class Image {
+
+    @Id
+    public String id;
+
+    public String username;
+    public String image; //image data stored as string
+
+    public Phrase() {}
+
+    public Phrase(String username, String phrase) {
+        this.username = username;
+        this.image = image;
+    }
+
+    public String getId() {
+	return this.id;
+    }
+
+    public String getUsername() {
+	return this.username;
+    }
+
+    public String getImage() {
+	return this.image;
+    }
+
+}
