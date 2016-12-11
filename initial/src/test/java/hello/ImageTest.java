a0e1cacf17d9a3eca28f0b4f5d8278c70ae37738
Zion Lin <zion.e.lin@gmail.com>
2016-12-10 22:20:39 -0500

    Increased coverage slightly

--- initial/src/test/java/hello/ImageTest.java
+++ initial/src/test/java/hello/ImageTest.java
@@ -42,7 +42,7 @@ public class ImageTest {
     public void contexLoads() throws Exception {
         //make sure that controller is being created
         assertThat(controller).isNotNull();
-        //assertNotNull("Check that we can get the image id", image.getId()); // autoassigned by spring
+        assertEquals("Check that we can get the image id", null, image.getId()); // autoassigned by spring
         assertEquals("Check that we get the correct username: ","zion", image.getUsername());
         assertEquals("Check that we get the correct image data: ", "123", image.getImage());
     }
